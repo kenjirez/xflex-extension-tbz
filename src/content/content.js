7bb984766ef68009f4211b0637c01a60fbf6b9bc
Marco <marco.nemeth@protonmail.com>
2024-02-01 21:54:08 +0100

    Add files via upload

--- src/content/content.js
+++ src/content/content.js
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         ticketInfos.id = document.querySelector('[data-test-id="ticket-human-display-id"]').innerText;
         ticketInfos.title = document.querySelector('[data-test-id="subject-text"]').innerText;
         ticketInfos.requester = document.querySelector("body").querySelector('#primary_email').textContent;
-        ticketInfos.company = document.querySelector('[aria-labelledby$="_departmentId"]').querySelector(".ember-power-select-selected-item").innerText;
+        ticketInfos.company = document.querySelector('[data-test-field-name="departmentId"]').querySelector(".ember-power-select-selected-item").innerText;
         ticketInfos.agent = document.querySelector('[aria-labelledby$="_responderId"]').querySelector(".ember-power-select-selected-item").innerText;
         
          // send response to background worker
